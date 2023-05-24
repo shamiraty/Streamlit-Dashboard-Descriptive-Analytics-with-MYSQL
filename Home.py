f07dafd08757d58ef540a747b681893b0bbca2dc
shamiraty <129072179+shamiraty@users.noreply.github.com>
2023-05-24 17:56:20 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -86,50 +86,58 @@ def Home():
 
 #graphs
 
-def graphs():
-    #total_investment=int(df_selection["Investment"]).sum()
-    #averageRating=int(round(df_selection["Rating"]).mean(),2)
-    
-    #simple bar graph
-    investment_by_business_type=(
-        df_selection.groupby(by=["BusinessType"]).count()[["Investment"]].sort_values(by="Investment")
-    )
-    fig_investment=px.bar(
-       investment_by_business_type,
-       x="Investment",
-       y=investment_by_business_type.index,
-       orientation="h",
-       title="<b> Investment by Business Type </b>",
-       color_discrete_sequence=["#0083B8"]*len(investment_by_business_type),
-       template="plotly_white",
-    )
+def Graphs():
+ total_investments = int(df_selection["Investment"].sum())
+ average_rating = round(df_selection["Rating"].mean(), 1)
+ star_rating = ":star:" * int(round(average_rating, 0))
+ average_investment = round(df_selection["Investment"].mean(), 2)
 
+#1. simple bar graph
+ investment_by_businessType = (
+    df_selection.groupby(by=["BusinessType"]).count()[["Investment"]].sort_values(by="Investment")
+ )
+ fig_investment = px.bar(
+    investment_by_businessType,
+    x="Investment",
+    y=investment_by_businessType.index,
+    orientation="h",
+    title="Investment by Business Type",
+    color_discrete_sequence=["#0083B8"] * len(investment_by_businessType),
+    template="plotly_white",
+ )
 
-    fig_investment.update_layout(
+ fig_investment.update_layout(
     plot_bgcolor="rgba(0,0,0,0)",
     xaxis=(dict(showgrid=False))
-     )
+ )
 
-        #simple line graph
-    investment_state=df_selection.groupby(by=["State"]).count()[["Investment"]]
-    fig_state=px.line(
-       investment_state,
-       x=investment_state.index,
-       y="Investment",
-       orientation="v",
-       title="<b> Investment by State </b>",
-       color_discrete_sequence=["#0083b8"]*len(investment_state),
-       template="plotly_white",
-    )
-    fig_state.update_layout(
+#2. simple line graph------------------
+ investment_by_state = df_selection.groupby(by=["State"]).count()[["Investment"]]
+ fig_state = px.line(
+    investment_by_state,
+    x=investment_by_state.index,
+     orientation="v",
+    y="Investment",
+    title="Investment by Region ",
+    color_discrete_sequence=["#0083B8"] * len(investment_by_state),
+    template="plotly_white",
+ )
+ fig_state.update_layout(
     xaxis=dict(tickmode="linear"),
     plot_bgcolor="rgba(0,0,0,0)",
-    yaxis=(dict(showgrid=False))
-     )
+    yaxis=(dict(showgrid=False)),
+ )
 
-    left,right=st.columns(2)
-    left.plotly_chart(fig_state,use_container_width=True)
-    right.plotly_chart(fig_investment,use_container_width=True)
+ left_column, right_column,center = st.columns(3)
+ left_column.plotly_chart(fig_state, use_container_width=True)
+ right_column.plotly_chart(fig_investment, use_container_width=True)
+
+ #pie chart
+ with center:
+  fig = px.pie(df_selection, values='Rating', names='State', title='Regions by Ratings')
+  fig.update_layout(legend_title="Regions", legend_y=0.9)
+  fig.update_traces(textinfo='percent+label', textposition='inside')
+  st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
      
 def Progressbar():
     st.markdown("""<style>.stProgress > div > div > div > div { background-image: linear-gradient(to right, #99ff99 , #FFFF00)}</style>""",unsafe_allow_html=True,)
