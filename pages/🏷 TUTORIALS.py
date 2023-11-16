1d9a893d42d2ecfae655b052fed01f42b193a978
shamiraty <129072179+shamiraty@users.noreply.github.com>
2023-11-16 11:46:08 +0300

    Add files via upload

---
+++ pages/🏷 TUTORIALS.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import webbrowser
+st.set_page_config(page_title="Tutorials", page_icon="📈", layout="wide")  
+
+
+with open('style.css')as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
+    
+with st.expander("🏷 Analytics Web application Multiple Linear Regression Web Project"):
+ if st.button("Watch Video 1"):
+    webbrowser.open_new_tab("https://youtu.be/VeP0c-ZPkD8")
+
+
+with st.expander("🏷 Python Web Dashboard using DashPlotly and Dash Bootstrap Component"):
+ if st.button("Watch Video 2"):
+    webbrowser.open_new_tab("https://youtu.be/5SbIGjhkez0")
+
+with st.expander("🏷 Business Analytics Website Dashboard "):
+ if st.button("Watch Video 3"):
+    webbrowser.open_new_tab("https://youtu.be/sIqBA0PhzGQ")
+
+
+with st.expander("🏷 Descriptive Statistics analytics web with MySQL"):
+ if st.button("Watch Video 4"):
+    webbrowser.open_new_tab("https://youtu.be/pWxDxhWXJos")
+
+with st.expander("🏷 Machine Learning Web App Multiple Linear Regression"):
+ if st.button("Watch Video 5"):
+    webbrowser.open_new_tab("https://youtu.be/4Y2oMUaKkGk")
+
+with st.expander("🏷 Analytics dashboard with Excel"):
+ if st.button("Watch Video 6"):
+    webbrowser.open_new_tab("https://youtu.be/-cg3qPhI74s")
+
+
+with st.expander("🏷 Web Analytics Dashboard Sales & Inventory"):
+ if st.button("Watch Video 7"):
+    webbrowser.open_new_tab("https://youtu.be/uDuPM4xfPlw")
+
+
+with st.expander("🏷 PygWalker Data Exploratory Library"):
+ if st.button("Watch Video 8"):
+    webbrowser.open_new_tab("https://youtu.be/JeoYZkmeYTo")
+
+with st.expander("🏷 Cross Matrix tabulation"):
+ if st.button("Watch Video 9"):
+    webbrowser.open_new_tab("https://youtu.be/1fnq4CzezxQ")
+
+
+with st.expander("🏷 Add new Record to Excel file via Web Interface"):
+ if st.button("Watch Video 10"):
+    webbrowser.open_new_tab("https://youtu.be/-3q2rwuy99g")
+
+
+with st.expander("🏷 Variable Distributions & Measure of Dispersion"):
+ if st.button("Watch Video 11"):
+    webbrowser.open_new_tab("https://youtu.be/U7vf-DB_KmQ")
+
+with st.expander("🏷 Business Intelligent Analytics Web Dashboard"):
+ if st.button("Watch Video 12"):
+    webbrowser.open_new_tab("https://www.youtube.com/watch?v=mDJ-sKB7DvE")
+
+
+with st.expander("🏷 Business Analytics Web Dashboard Using Python, Html, CSS"):
+ if st.button("Watch Video 13"):
+    webbrowser.open_new_tab("https://youtu.be/rJzBNm0rb0g")
+
+ 
