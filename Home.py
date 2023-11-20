08106adfd797915a1a5230f8865adddccd3485f4
shamiraty <129072179+shamiraty@users.noreply.github.com>
2023-11-20 18:19:26 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -54,7 +54,165 @@ df_selection=df.query(
     "Region==@region & Location==@location & Construction ==@construction"
 )
 
+#this function performs basic descriptive analytics like Mean,Mode,Sum  etc
+def Home():
+    with st.expander("VIEW EXCEL DATASET"):
+        showData=st.multiselect('Filter: ',df_selection.columns,default=["Policy","Expiry","Location","State","Region","Investment","Construction","BusinessType","Earthquake","Flood","Rating"])
+        st.dataframe(df_selection[showData],use_container_width=True)
+    #compute top analytics
+    total_investment = float(df_selection['Investment'].sum())
+    investment_mode = float(df_selection['Investment'].mode())
+    investment_mean = float(df_selection['Investment'].mean())
+    investment_median= float(df_selection['Investment'].median()) 
+    rating = float(df_selection['Rating'].sum())
 
+    total1,total2,total3,total4,total5=st.columns(5,gap='small')
+    with total1:
+        st.info('Total Sum Investment',icon="💰")
+        st.metric(label="Sum TZS",value=f"{total_investment:,.0f}")
+
+    with total2:
+        st.info('Most Frequent Investment',icon="💰")
+        st.metric(label="Mode TZS",value=f"{investment_mode:,.0f}")
+
+    with total3:
+        st.info('Average Mean',icon="💰")
+        st.metric(label="Average TZS",value=f"{investment_mean:,.0f}")
+
+    with total4:
+        st.info('Central Earnings',icon="💰")
+        st.metric(label="Median TZS",value=f"{investment_median:,.0f}")
+
+    with total5:
+        st.info('Ratings',icon="💰")
+        st.metric(label="Rating",value=numerize(rating),help=f""" Total Rating: {rating} """)
+    style_metric_cards(background_color="#FFFFFF",border_left_color="#686664",border_color="#000000",box_shadow="#F71938")
+
+    #variable distribution Histogram
+    with st.expander("EXPLORATORY VARIABLE DISTRIBUTIONS BY FREQUENCY"):
+     df.hist(figsize=(16,8),color='#898784', zorder=2, rwidth=0.9,legend = ['Investment']);
+     st.pyplot()
+
+#graphs
+def graphs():
+    #total_investment=int(df_selection["Investment"]).sum()
+    #averageRating=int(round(df_selection["Rating"]).mean(),2) 
+    #simple bar graph  investment by business type
+    investment_by_business_type=(
+        df_selection.groupby(by=["BusinessType"]).count()[["Investment"]].sort_values(by="Investment")
+    )
+    fig_investment=px.bar(
+       investment_by_business_type,
+       x="Investment",
+       y=investment_by_business_type.index,
+       orientation="h",
+       title="<b> INVESTMENT BY BUSINESS TYPE </b>",
+       color_discrete_sequence=["#0083B8"]*len(investment_by_business_type),
+       template="plotly_white",
+    )
+    fig_investment.update_layout(
+     plot_bgcolor="rgba(0,0,0,0)",
+     font=dict(color="black"),
+     yaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show y-axis grid and set its color  
+     paper_bgcolor='rgba(0, 0, 0, 0)',  # Set paper background color to transparent
+     xaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show x-axis grid and set its color
+     )
+
+    #simple line graph investment by state
+    investment_state=df_selection.groupby(by=["State"]).count()[["Investment"]]
+    fig_state=px.line(
+       investment_state,
+       x=investment_state.index,
+       y="Investment",
+       orientation="v",
+       title="<b> INVESTMENT BY STATE </b>",
+       color_discrete_sequence=["#0083b8"]*len(investment_state),
+       template="plotly_white",
+    )
+    fig_state.update_layout(
+    xaxis=dict(tickmode="linear"),
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=(dict(showgrid=False))
+     )
+
+    left,right,center=st.columns(3)
+    left.plotly_chart(fig_state,use_container_width=True)
+    right.plotly_chart(fig_investment,use_container_width=True)
+    
+    with center:
+      #pie chart
+      fig = px.pie(df_selection, values='Rating', names='State', title='RATINGS BY REGIONS')
+      fig.update_layout(legend_title="Regions", legend_y=0.9)
+      fig.update_traces(textinfo='percent+label', textposition='inside')
+      st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+
+#function to show current earnings against expected target     
+def Progressbar():
+    st.markdown("""<style>.stProgress > div > div > div > div { background-image: linear-gradient(to right, #99ff99 , #FFFF00)}</style>""",unsafe_allow_html=True,)
+    target=3000000000
+    current=df_selection["Investment"].sum()
+    percent=round((current/target*100))
+    mybar=st.progress(0)
+
+    if percent>100:
+        st.subheader("Target done !")
+    else:
+     st.write("you have ",percent, "% " ,"of ", (format(target, 'd')), "TZS")
+     for percent_complete in range(percent):
+        time.sleep(0.1)
+        mybar.progress(percent_complete+1,text=" Target Percentage")
+
+#menu bar
+def sideBar():
+ with st.sidebar:
+    selected=option_menu(
+        menu_title="Main Menu",
+        options=["Home","Progress"],
+        icons=["house","eye"],
+        menu_icon="cast",
+        default_index=0
+    )
+ if selected=="Home":
+    #st.subheader(f"Page: {selected}")
+    Home()
+    graphs()
+ if selected=="Progress":
+    #st.subheader(f"Page: {selected}")
+    Progressbar()
+    graphs()
+
+sideBar()
+st.sidebar.image("data/logo1.png",caption="")
+
+
+st.subheader('PICK FEATURES TO EXPLORE DISTRIBUTIONS TRENDS BY QUARTILES',)
+#feature_x = st.selectbox('Select feature for x Qualitative data', df_selection.select_dtypes("object").columns)
+feature_y = st.selectbox('Select feature for y Quantitative Data', df_selection.select_dtypes("number").columns)
+fig2 = go.Figure(
+    data=[go.Box(x=df['BusinessType'], y=df[feature_y])],
+    layout=go.Layout(
+        title=go.layout.Title(text="BUSINESS TYPE BY QUARTILES OF INVESTMENT"),
+        plot_bgcolor='rgba(0, 0, 0, 0)',  # Set plot background color to transparent
+        paper_bgcolor='rgba(0, 0, 0, 0)',  # Set paper background color to transparent
+        xaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show x-axis grid and set its color
+        yaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show y-axis grid and set its color
+        font=dict(color='#cecdcd'),  # Set text color to black
+    )
+)
+# Display the Plotly figure using Streamlit
+st.plotly_chart(fig2,use_container_width=True)
+
+
+
+#theme
+hide_st_style=""" 
+
+<style>
+#MainMenu {visibility:hidden;}
+footer {visibility:hidden;}
+header {visibility:hidden;}
+</style>
+"""
 
 
 
